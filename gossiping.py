3da5da1d3b7de83c350b28f299f25e492a430744
cw chen <sky85245630@gmail.com>
2020-07-29 22:37:49 +0800

    get time

--- gossiping.py
+++ gossiping.py
@@ -1,6 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-import html5lib
+# import html5lib
+import time
 
 resp = requests.get('https://www.ptt.cc/bbs/Gossiping/index.html')
 soup = BeautifulSoup(resp.text, 'html5lib')
+
+#先印出時間，再把前面的0拿掉
+today = time.strftime('%m/%d').lstrip('0')
+print(today)
+
