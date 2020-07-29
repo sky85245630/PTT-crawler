47833dc2448154ab7ebd185879510145d585061d
cw chen <sky85245630@gmail.com>
2020-07-29 22:42:40 +0800

    url != 200

--- gossiping.py
+++ gossiping.py
@@ -3,10 +3,19 @@ from bs4 import BeautifulSoup
 # import html5lib
 import time
 
-resp = requests.get('https://www.ptt.cc/bbs/Gossiping/index.html')
-soup = BeautifulSoup(resp.text, 'html5lib')
+
 
 #先印出時間，再把前面的0拿掉
 today = time.strftime('%m/%d').lstrip('0')
 print(today)
+
+def Gossiping(url):
+    resp = requests.get(url)
+    # URL錯誤回報
+    if resp.status_code != 200:
+        print('URL發生錯誤' + url)
+        return
+
+
+
 
